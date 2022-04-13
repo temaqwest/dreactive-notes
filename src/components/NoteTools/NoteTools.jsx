e5f7ba1eb6ec8bea02b2b2aa6d36d60c83e52c1d
temaqwest <taylorartem95@gmail.com>
2022-04-13 21:03:02 +0300

    New components: - NoteTools - Clock Modified components: - Header (logo, title, clock) - Button (styles, new props etc) - Icon (styles) - NoteContent - AsideMenu (Delete button, new styles) - AsideMenuItem (New props, markup, logic)
    Extra:
    - new icons in icon-sprite.svg
    - new logo.png

---
+++ src/components/NoteTools/NoteTools.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import classes from './NoteTools.module.css';
+import Button from "../UI/Button/Button";
+import Icon from "../UI/Icon/Icon";
+
+const NoteTools = () => {
+    const [tools] = useState([
+        { id: 1, name: 'H1', description: 'Заголовок первого уровня' },
+        { id: 2, name: 'H2', description: 'Заголовок второго уровня' },
+        { id: 3, name: 'H3', description: 'Заголовок третьего уровня' },
+        { id: 4, name: 'P', description: 'Параграф' },
+        { id: 5, name: 'Ul', iconID: 'ullist', description: 'Список с маркерами' },
+        { id: 6, name: 'Ol', iconID: 'ollist', description: 'Пронумерованный список' },
+    ]);
+
+    return (
+        <div className={classes.note__tools}>
+            {
+                tools.map((tool) => {
+                    return (
+                        <Button
+                            type="button"
+                            key={tool.id}
+                            onClick={(e) => e.stopPropagation()}
+                            title={tool.description}
+                            data-name={tool.name}
+                            modifier={classes.note__tool}
+                        >
+                            {
+                                tool.iconID ?
+                                    <Icon name={tool.iconID}/> :
+                                    tool.name
+                            }
+                        </Button>
+                    )
+                })
+            }
+        </div>
+    );
+};
+
+export default NoteTools;
